729bde6853afcb96e0cdf724c6d3386a994723e7
Tashpolotov <Aza662485@gmail.com>
2022-11-25 12:16:28 +0600

    Initial commit

---
+++ app/src/main/java/com/example/home_work_3_5/DetailsFragment.java
@@ -0,0 +1,39 @@
+package com.example.home_work_3_5;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailsFragment extends Fragment implements OnClickListener{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (getArguments() != null) {
+
+            getArguments().getInt("name");
+
+        }
+    }
+
+    @Override
+    public void onItemClickListener(ChatModel chatModel) {
+
+    }
+}
